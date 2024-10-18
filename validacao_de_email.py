c49bfe8c1e000d79994caabec25cd1a6bfafab6b
Kayky Queiroz <kaykysousa617@gmail.com>
2024-10-18 16:17:24 -0300

    first commit

---
+++ validacao_de_email.py
@@ -0,0 +1,24 @@
+"""
+Como verificar se um e-mail é válido?
+Se a solicitação chegar ao endereço de e-mail dos destinatários e receber uma resposta, 
+o e-mail é considerado ativo e você pode enviar e-mails reais com segurança para ele. 
+Isso também é conhecido como autenticação SMTP.
+"""
+import re
+
+def validar_email(email):
+    padrao_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    
+    if re.match(padrao_email,email):
+        return True
+    else:
+        return False
+    
+emails = [
+    'valido@gmail.com.br',
+    "invalido.com"
+]
+
+
+for email in emails:
+    print(validar_email(email))
